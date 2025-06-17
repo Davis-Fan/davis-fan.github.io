5497d85e58f6340880424526f942027bb40fd7a6
Davis Fan <davis@DavisdeMacBook-Pro.local>
2025-06-17 18:14:11 -0400

    Initial commit

--- run-serve.sh
+++
@@ -1,1 +0,0 @@
-bundle exec jekyll serve 2>&1 | egrep -v 'deprecated'
--- update-website.sh
+++
@@ -1,4 +0,0 @@
-bundle exec jekyll build
-git add _site
-git commit -am "Update website"
-git subtree push --prefix _site git@github.com:artix41/artix41.github.io.git master
